3dd458340a7188afc76a7dbeb6331cd8986cd460
ruuuh20 <pkim2320@gmail.com>
2022-03-04 18:32:55 -0500

    editing about

--- pages/about.js
+++ pages/about.js
@@ -19,7 +19,7 @@ import ImageStandard from "@/components/imageStandard";
 import ImageWrapper from "@/components/imageWrapper";
 import ImageComponent from "@/components/image";
 
-import Img from "next/image";
+import Image from "next/image";
 import { useNextSanityImage } from "next-sanity-image";
 
 export default function About(props) {
@@ -36,8 +36,6 @@ export default function About(props) {
     <Layout>
       <NextSeo title="About" />
 
-      <Header />
-
       <LazyMotion features={domAnimation}>
         <m.div
           initial="initial"
@@ -52,13 +50,13 @@ export default function About(props) {
                   <>
                     <Container>
                       <div className="max-w-3xl  relative mb-16 md:mb-20 2xl:mb-28 mx-[3%] md:mx-[5%] lg:mx-24 2xl:mx-32">
-                        <div className="block overflow-hidden mb-4 md:mb-6 2xl:mb-8">
+                        <div className="block mb-4 overflow-hidden md:mb-6 2xl:mb-8">
                           <div className="relative mb-2 md:mb-3">
-                            <h2 className="md:text-lg uppercase block ">
+                            <h2 className="block uppercase md:text-lg ">
                               {post.title}
                             </h2>
                           </div>
-                          <div className="text-2xl md:text-3xl xl:text-4xl w-full pr-12">
+                          <div className="w-full pr-12 text-2xl md:text-3xl xl:text-4xl">
                             <p className="font-bold leading-snug">
                               {post.introText}
                             </p>
@@ -69,21 +67,39 @@ export default function About(props) {
 
                     <Container>
                       <div className="flex flex-wrap">
-                        <div className="w-full md:w-1/2 content md:px-12 xl:px-16 max-w-2xl">
+                        <div className="w-full max-w-2xl md:w-1/2 content md:px-12 xl:px-16">
                           <div className="content">
                             <p>{post.foundersMessage}</p>
+                            <p>oijasoiad</p>
+                            <p>oijasoiad</p>
+                            <p>oijasoiad</p>
+                            <p>oijasoiad</p>
+                            <p>oijasoiad</p>
+                            <p>oijasoiad</p>
+                            <p>oijasoiad</p>
                           </div>
                         </div>
-                        <div className="w-full md:w-1/2 relative content mb-8 md:mb-0 overflow-hidden">
-                          <figure className="mb-6 md:mb-8 xl:mb-10 rounded-full overflow-hidden">
-                            <ImageComponent
-                              image={
-                                post.foundersImageUrl !== null
-                                  ? post.foundersImageUrl
-                                  : "https://via.placeholder.com/50"
-                              }
-                            />
-                          </figure>
+                        <div className="relative w-full mb-8 overflow-hidden md:w-1/2 content md:mb-0">
+                          <div className="image-container">
+                            <figure className="mb-6 overflow-hidden rounded-full md:mb-8 xl:mb-10">
+                              <Image
+                                className="image-circle"
+                                // layout="fill"
+                                // width={400}
+                                // height={400}
+                                // width={800}
+                                // height={450}
+                                // layout="responsive"
+                                layout="fill"
+                                objectFit="contain"
+                                src={
+                                  post.foundersImageUrl !== null
+                                    ? post.foundersImageUrl
+                                    : "https://via.placeholder.com/50"
+                                }
+                              />
+                            </figure>
+                          </div>
                         </div>
                       </div>
                     </Container>
@@ -91,113 +107,112 @@ export default function About(props) {
                     {/* flex 2 columns */}
                     <Container className="my-2">
                       <div className="relative mb-2 md:mb-3">
-                        <h2 className="md:text-lg uppercase block ">
+                        <h2 className="block uppercase md:text-lg ">
                           Our History
                         </h2>
                       </div>
                       {post.timelineItems?.map((item, i) => {
                         return (
                           <>
-                            <div className="flex flex-wrap md-mx-6 border-t border-opacity-50 md:py-8 py-5">
+                            <div className="flex flex-wrap py-5 border-t border-opacity-50 md-mx-6 md:py-8">
                               <div className="w-full md:w-7/12 xl:w-2/6 md:px-6 md:mb-0">
                                 <div className="max-w-xl">
-                                  <div className="text-2xl md:text-3xl xl:text-4xl w-full text-right">
+                                  <div className="w-full text-2xl text-right md:text-3xl xl:text-4xl">
                                     <p>{item.timelineItemYear}</p>
                                   </div>
                                 </div>
                               </div>
-                              <div className="w-full md:w-7/12 xl:w-4/6 md:pl-10 ml-auto leading-8">
+                              <div className="w-full ml-auto leading-8 md:w-7/12 xl:w-4/6 md:pl-10">
                                 <p>{item.timelineItemText}</p>
                               </div>
                             </div>
                           </>
                         );
                       })}
-                      a
                     </Container>
                     <hr />
                     <Container>
                       <div className="relative mb-2 md:mb-3">
-                        <h2 className="md:text-lg uppercase block ">
+                        <h2 className="block uppercase md:text-lg ">
                           Our Team / Leadership
                         </h2>
                       </div>
-                      <div className="grid gap-4 md:gap-6 grid-cols-2 md:grid-cols-3 mb-16 md:mb-24 2xl:mb-32 relative z-20 md:mx-16 2xl:mx-24">
+                      <div className="relative z-20 grid grid-cols-2 gap-4 mb-16 md:gap-6 md:grid-cols-3 md:mb-24 2xl:mb-32 md:mx-16 2xl:mx-24">
                         <div className="w-full mb-3 md:mb-5 2xl:mb-8">
                           <div className="border-blue border-2 mb-3 md:mb-4 bg-pink h-[50vw] md:h-[27vw] lg:h-[29vw] xl:h-[33vw] 2xl:h-[31.5vw] max-h-[480px] relative team-image">
-                            <div className="absolute top-0 left-0 bottom-0 right-0 overflow-hidden">
+                            <div className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden">
                               image
                             </div>
                           </div>
-                          <h4 className="mb-0 pb-0 text-lg md:text-xl 2xl:text-2xl font-display block mb-0 pb-0 leading-tight">
+                          <h4 className="block pb-0 mb-0 text-lg leading-tight md:text-xl 2xl:text-2xl font-display">
                             Joe Smith
                           </h4>
-                          <span className="italic text-xs md:text-sm">
+                          <span className="text-xs italic md:text-sm">
                             Member
                           </span>
                         </div>
                         <div className="w-full mb-3 md:mb-5 2xl:mb-8">
                           <div className="border-blue border-2 mb-3 md:mb-4 bg-pink h-[50vw] md:h-[27vw] lg:h-[29vw] xl:h-[33vw] 2xl:h-[31.5vw] max-h-[480px] relative team-image">
-                            <div className="absolute top-0 left-0 bottom-0 right-0 overflow-hidden">
+                            <div className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden">
                               image
                             </div>
                           </div>
-                          <h4 className="mb-0 pb-0 text-lg md:text-xl 2xl:text-2xl font-display block mb-0 pb-0 leading-tight">
+                          <h4 className="block pb-0 mb-0 text-lg leading-tight md:text-xl 2xl:text-2xl font-display">
                             Joe Smith
                           </h4>
-                          <span className="italic text-xs md:text-sm">
+                          <span className="text-xs italic md:text-sm">
                             Member
                           </span>
                         </div>
                         <div className="w-full mb-3 md:mb-5 2xl:mb-8">
                           <div className="border-blue border-2 mb-3 md:mb-4 bg-pink h-[50vw] md:h-[27vw] lg:h-[29vw] xl:h-[33vw] 2xl:h-[31.5vw] max-h-[480px] relative team-image">
-                            <div className="absolute top-0 left-0 bottom-0 right-0 overflow-hidden">
+                            <div className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden">
                               image
                             </div>
                           </div>
-                          <h4 className="mb-0 pb-0 text-lg md:text-xl 2xl:text-2xl font-display block mb-0 pb-0 leading-tight">
+                          <h4 className="block pb-0 mb-0 text-lg leading-tight md:text-xl 2xl:text-2xl font-display">
                             Joe Smith
                           </h4>
-                          <span className="italic text-xs md:text-sm">
+                          <span className="text-xs italic md:text-sm">
                             Member
                           </span>
                         </div>
                         <div className="w-full mb-3 md:mb-5 2xl:mb-8">
                           <div className="border-blue border-2 mb-3 md:mb-4 bg-pink h-[50vw] md:h-[27vw] lg:h-[29vw] xl:h-[33vw] 2xl:h-[31.5vw] max-h-[480px] relative team-image">
-                            <div className="absolute top-0 left-0 bottom-0 right-0 overflow-hidden">
+                            <div className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden">
                               image
                             </div>
                           </div>
-                          <h4 className="mb-0 pb-0 text-lg md:text-xl 2xl:text-2xl font-display block mb-0 pb-0 leading-tight">
+                          <h4 className="block pb-0 mb-0 text-lg leading-tight md:text-xl 2xl:text-2xl font-display">
                             Joe Smith
                           </h4>
-                          <span className="italic text-xs md:text-sm">
+                          <span className="text-xs italic md:text-sm">
                             Member
                           </span>
                         </div>
                         <div className="w-full mb-3 md:mb-5 2xl:mb-8">
                           <div className="border-blue border-2 mb-3 md:mb-4 bg-pink h-[50vw] md:h-[27vw] lg:h-[29vw] xl:h-[33vw] 2xl:h-[31.5vw] max-h-[480px] relative team-image">
-                            <div className="absolute top-0 left-0 bottom-0 right-0 overflow-hidden">
+                            <div className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden">
                               image
                             </div>
                           </div>
-                          <h4 className="mb-0 pb-0 text-lg md:text-xl 2xl:text-2xl font-display block mb-0 pb-0 leading-tight">
+                          <h4 className="block pb-0 mb-0 text-lg leading-tight md:text-xl 2xl:text-2xl font-display">
                             Joe Smith
                           </h4>
-                          <span className="italic text-xs md:text-sm">
+                          <span className="text-xs italic md:text-sm">
                             Member
                           </span>
                         </div>
                         <div className="w-full mb-3 md:mb-5 2xl:mb-8">
                           <div className="border-blue border-2 mb-3 md:mb-4 bg-pink h-[50vw] md:h-[27vw] lg:h-[29vw] xl:h-[33vw] 2xl:h-[31.5vw] max-h-[480px] relative team-image">
-                            <div className="absolute top-0 left-0 bottom-0 right-0 overflow-hidden">
+                            <div className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden">
                               image
                             </div>
                           </div>
-                          <h4 className="mb-0 pb-0 text-lg md:text-xl 2xl:text-2xl font-display block mb-0 pb-0 leading-tight">
+                          <h4 className="block pb-0 mb-0 text-lg leading-tight md:text-xl 2xl:text-2xl font-display">
                             Joe Smith
                           </h4>
-                          <span className="italic text-xs md:text-sm">
+                          <span className="text-xs italic md:text-sm">
                             Member
                           </span>
                         </div>
---
+++ pages/programs/index.js
@@ -0,0 +1,97 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import client, {
+  getClient,
+  usePreviewSubscription,
+  PortableText,
+} from "../../sanity";
+
+import { groq } from "next-sanity";
+import Layout from "@/components/layout";
+import Header from "@/components/header";
+import Container from "@/components/container";
+import Link from "next/link";
+import ImageComponent from "@/components/image";
+import ImageStandard from "@/components/imageStandard";
+import { LazyMotion, domAnimation, m } from "framer-motion";
+import { NextSeo } from "next-seo";
+
+export default function Programs(props) {
+  const { postdata, preview } = props;
+
+  const router = useRouter();
+
+  const { data: posts } = usePreviewSubscription(query, {
+    initialData: postdata,
+    enabled: preview || router.query.preview !== undefined,
+  });
+
+  return (
+    <>
+      <Layout>
+        <NextSeo title="About" />
+
+        <Container>
+          <div className="relative w-full pt-8 pb-[88px]">
+            <h2 className="relative block pb-0 pr-12 mb-0 text-2xl font-light leading-normal tracking-tight text-gray-400 md:text-3xl lg:text-4xl 2xl:text-5xl">
+              Programs
+            </h2>
+            <span className="relative block pb-0 pr-12 mb-0 text-4xl tracking-tight md:text-5xl lg:text-6xl 2xl:text-6xl">
+              Our Current Programs
+            </span>
+          </div>
+
+          <div className="relative">
+            {posts &&
+              posts.map((post) => (
+                <Link href={`/${post.slug}`}>
+                  <a className="flex flex-wrap m-auto border-b group md:py-10 md:w-full">
+                    <div className="w-full">
+                      <ImageComponent
+                        image={
+                          post.mainImage !== null
+                            ? post.mainImage.url
+                            : "https://via.placeholder.com/50"
+                        }
+                      />
+                    </div>
+
+                    <div className="flex justify-center w-full bg-[#e1d8dc]">
+                      <div className="p-4 intro">
+                        <h3 className="block w-full text-2xl duration-500 ease-in-out md:text-3xl group-hover:opacity-60">
+                          {post.title}
+                        </h3>
+                        <p>{post.description}</p>
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              ))}
+          </div>
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+const query = groq`
+*[_type == "program"] | order(_createdAt desc) {
+  ..., 
+  mainImage {
+  ...asset->
+},
+ 
+}
+`;
+
+export async function getStaticProps({ params, preview = false }) {
+  const contactData = await getClient(preview).fetch(query);
+
+  return {
+    props: {
+      postdata: contactData,
+      preview,
+    },
+    revalidate: 10,
+  };
+}
