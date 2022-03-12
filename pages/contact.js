5caf40b4e0ff76fcfd67dd122cc7fdb93892e684
ruuuh20 <pkim2320@gmail.com>
2022-03-11 22:13:28 -0500

    fixing contact page

--- pages/contact.js
+++ pages/contact.js
@@ -5,29 +5,63 @@ import client, {
   usePreviewSubscription,
   PortableText,
 } from "../sanity";
-
+import { NextSeo } from "next-seo";
 import { groq } from "next-sanity";
+import Layout from "@/components/layout";
+import Container from "@/components/container";
 
-export default function Post(props) {
+export default function Contact(props) {
   const { postdata, preview } = props;
 
   const router = useRouter();
+  const locale = router.locale || router.defaultLocale;
 
   const { data: posts } = usePreviewSubscription(query, {
     initialData: postdata,
     enabled: preview || router.query.preview !== undefined,
   });
-  console.log(posts)
+
   return (
     <>
-      {posts &&
-        posts.map((post) => (
-          <article>
-            <h3 className="text-lg"> {post.title} </h3>
-            <h3 className="text-lg"> {post.address} </h3>
-            
-          </article>
-        ))}
+      <Layout>
+        <NextSeo title="Contact Us" />
+        <Container>
+          <div className="relative w-full pt-8 pb-[88px]">
+            <span className="relative block pb-0 pr-12 mb-0 text-4xl tracking-tight md:text-5xl lg:text-6xl 2xl:text-6xl">
+              Contact Us
+            </span>
+          </div>
+        </Container>
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="w-full p-6 lg:w-7/12">
+              <p className="text-xl">
+                We’d love to hear from you. For all inquiries or feedback, send
+                us an email at{" "}
+                <a
+                  className="border-b border-indigo-500 hover:text-indigo-500"
+                  href="mailto:"
+                >
+                  {posts && posts[0].email}
+                </a>{" "}
+                or fill out the form below.
+              </p>{" "}
+            </div>
+            <div className="w-full lg:w-5/12 p-6">
+              <div className="box bg-blue-light p-4">
+                {posts &&
+                  posts.map((post) => (
+                    <ul>
+                      <li className="text-lg mb-2"> {post.email} </li>
+                      <li className="text-lg mb-2"> {post.address} </li>
+                      <li>socials</li>
+                    </ul>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </Layout>
     </>
   );
 }
@@ -42,7 +76,6 @@ const query = groq`
 
 export async function getStaticProps({ params, preview = false }) {
   const contactData = await getClient(preview).fetch(query);
- 
 
   return {
     props: {
